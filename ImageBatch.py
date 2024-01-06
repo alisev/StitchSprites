cd6407e82dbaf7844a8baa110b6e8254cca036fc
Samwell <samlikesgreeneggs@gmail.com>
2024-01-06 15:08:35 -0700

    Added command line functionality to ImageBatch. Added transparency support for sprites. Forced sprites to be powers of two in width and height. Minor code tweaks.

--- ImageBatch.py
+++ ImageBatch.py
@@ -1,45 +1,37 @@
+import args
+import sys
+
 import os
 from re import split
 from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
+from progress.bar import Bar
 
-# takes a batch of sprites, crops them and stiches them into a single image
+
 class ImageBatch():
     root = ""
     paths = []
-    bg_color = ()
 
-    def __init__(self, dir: str, bg_color: tuple = None, sort_natural: bool = True):
+    def __init__(self, dir: str, sort_natural: bool = True):
         self.root = dir
         self.paths = self.__load_filenames(dir, sort_natural)
-        self.bg_color = self.__set_BG_color(bg_color)
 
-    def __load_filenames(self, dir: str, sort_natural: bool) -> list:
+    def __is_png(self, file : str) -> bool:
+        return file.endswith(".png")
+    
+    def __load_filenames(self, dir: str, sort_natural: bool = True) -> list:
         """
         Loads all filenames found in directory dir.
-        Natural sorting takes number order into account, e.g. 1_000.bmp, 2_000.bmp, 10_000.bmp instead of 1_000.bmp, 10_000.bmp, 2_000.bmp.
+        Natural sorting takes number order into account, 
+        e.g. 1_000.bmp, 2_000.bmp, 10_000.bmp instead of 
+        1_000.bmp, 10_000.bmp, 2_000.bmp.
         """
-        paths = os.listdir(dir)
+        paths = filter(self.__is_png, os.listdir(dir))
         if sort_natural:
             natural_sort_key = lambda s: [int(text) if text.isdigit() else text.lower() for text in split('([0-9]+)', s)]
             return sorted(paths, key = natural_sort_key)
         return paths
-    
-    def __set_BG_color(self, bg_color) -> tuple:
-        """
-        Checks value of bg_color and sets self.bg_color accordingly.
-        """
-        if bg_color == None: # TODO find a robust solution. For now this simply works.
-            image_path = self.get_image_path(0)
-            image = Image.open(image_path).convert('RGB')
-            color = image.getpixel((0, 0))
-            image.close()
-            return color
-        elif type(bg_color) == tuple:
-            return bg_color
-        else:
-            raise TypeError(self.__err_TypeError_message(bg_color, self.__set_BG_color, [tuple, None]))
 
     def get_image_path(self, identifier: any) -> str:
         """
@@ -53,7 +45,7 @@ class ImageBatch():
         elif type(identifier) == str:
             image_path = identifier
         else:
-            raise TypeError(self.__err_TypeError_message(identifier, self.__set_BG_color, [int, str]))
+            raise TypeError(self.__err_TypeError_message(identifier, [int, str]))
         return os.path.join(self.root, image_path)
     
     def show_image(self, image: Image):
@@ -65,7 +57,7 @@ class ImageBatch():
         plt.axis('off')
         plt.show()
     
-    def batch_process(self, image_fn, args: tuple = (), every: int = 50) -> list:
+    def batch_process(self, image_fn, args: tuple = (), every: int = 5) -> list:
         """
         Does a batch processing on images.
         Returns a list of values from the process function.
@@ -74,15 +66,37 @@ class ImageBatch():
         every - Used to show user how many images have been processed.
         """
         values = []
-        print(f"{image_fn.__name__}: Processing images...")
+        bar = Bar(f"{image_fn.__name__}", max = len(self.paths))
         for i in range(len(self.paths)):
             full_path = self.get_image_path(self.paths[i])
             image = Image.open(full_path)
             value = image_fn(*(image, *args))
             values.append(value)
             image.close()
-            self.__progress_message(i, every) # TODO replace with a progressbar
+            bar.next()
+        bar.finish()
         return values
+
+    def __scoot(self, A, rev=False):
+        """
+        Takes an array A and returns the index of the first non-trivial
+        enty. If rev is True, instead returns the index of the last
+        non-trivial entry.
+        A - a list
+        rev - True reverses direction of search
+        """
+        if rev:
+            direction = -1
+            index = len(A) - 1
+        else:
+            direction = 1
+            index = 0
+        if not A:
+            return float('inf') * direction
+
+        while not A[index]:
+            index += direction
+        return index
     
     def find_borders(self, image: Image) -> tuple:
         """"
@@ -90,15 +104,10 @@ class ImageBatch():
         Returns a tuple of bounding box or None if sprite is not found.
         image - PIL image to find borders for.
         """
-        # TODO needs some way to see previously saved min and max values and compare them
-        # doesn't work correctly in Palette mode, so its changed to RGB
-        mask = self.__get_pixel_mask(image)
-        indices = np.argwhere(mask)
-        if len(indices) > 0:
-            min_y, min_x = indices.min(axis=0)
-            max_y, max_x = indices.max(axis=0)
-            return min_x, min_y, max_x, max_y
-        return None
+        proj =  image.getchannel("A").getprojection()
+        minx, maxx = self.__scoot(proj[0]), self.__scoot(proj[0], rev = True)
+        miny, maxy = self.__scoot(proj[1]), self.__scoot(proj[1], rev = True)
+        return (minx, miny, maxx, maxy)
     
     def minmax_borders(self, borders: list) -> tuple:
         """
@@ -111,7 +120,22 @@ class ImageBatch():
         borders_arr = np.array(borders)
         mins = np.min(borders_arr, axis=0)
         maxs = np.max(borders_arr, axis=0)
-        return mins[0], mins[1], maxs[2], maxs[3]
+
+        # Ideally, width and height will be powers of two
+        width = maxs[0] - mins[0]
+        height = maxs[1] - mins[1]
+        niceWidth = 1
+        niceHeight = 1
+        while niceWidth < width:
+            niceWidth *= 2
+        while niceHeight < height:
+            niceHeight *= 2
+        minx = mins[0] - (niceWidth - width)/2
+        miny = mins[1] - (niceHeight - height)/2
+        maxx = minx + niceWidth
+        maxy = miny + niceHeight
+            
+        return minx, miny, maxx, maxy
     
     def __progress_message (self, i, every = 50):
         """
@@ -120,15 +144,6 @@ class ImageBatch():
         if i % every == 0:
             print(f"{i}/{len(self.paths)} images processed.")
     
-    def __get_pixel_mask(self, image: Image) -> np.ndarray:
-        """
-        For each pixel returns True if it belongs to the figure and False for background.
-        image - PIL Image
-        """
-        rgb_image = image.convert('RGB')
-        image_arr = np.array(rgb_image)
-        return np.all(image_arr != self.bg_color, axis=-1)
-
     def crop_image(self, image: Image, min_x: int, min_y: int, max_x: int, max_y: int) -> Image:
         """
         Crops image with given parameters
@@ -141,15 +156,16 @@ class ImageBatch():
         images - List of images to be stitched together
         x, y - Describes how many images there should be along x and y axis.
         """
+        # Make the system smarter. Perhaps save multiple files
+        # if x and y are too small.
         image_size = images[0].size
         canvas = image_size[0] * x, image_size[1] * y
-        new_image = Image.new('RGB', canvas, self.bg_color)
+        new_image = Image.new('RGBA', canvas, 0)
         for i in range(x):
             for j in range(y):
                 p = x * j + i
                 if(p >= len(images)):
                     break
-                images[p].convert('RGB')
                 new_x, new_y = image_size[0] * i, image_size[1] * j
                 new_image.paste(images[p], (new_x, new_y))
         return new_image
@@ -162,9 +178,47 @@ class ImageBatch():
         image.save(save_dir)
         print(f"Image {name} saved!")
 
-    def __err_TypeError_message(self, var: any, fn: function, expected_types: list) -> str:
+    def __err_TypeError_message(self, var: any, fn, expected_types: list) -> str:
         """
         Returns text for TypeError message.
         """
         return f"Wrong value type for variable in {fn.__name__}! Expected {expected_types}, got {type(var)}."
 
+
+if __name__ == "__main__":
+    try:
+        if len(sys.argv) < 2:
+            print("Usage: ImageBatch directory [out] [cols] [rows]")
+            sys.exit(1)
+
+        FOLDER = sys.argv[1]
+        DIR = os.path.join(os.getcwd(), FOLDER)
+
+        COLS, ROWS = 8, 1
+        OUTPUT = "spriteSheet.png"
+
+        if len(sys.argv) >= 3:
+            OUTPUT = sys.argv[2]
+
+        if len(sys.argv) >= 4:
+            if sys.argv[3].isdigit():
+                COLS = int(sys.argv[3])
+            else:
+                print("cols must be an integer")
+                sys.exit(1)
+
+        if len(sys.argv) >= 5:
+            if sys.argv[4].isdigit():
+                ROWS = int(sys.argv[4])
+            else:
+                print("rows must be an integer")
+                sys.exit(1)
+
+        images = ImageBatch(DIR)
+        border_list = images.batch_process(images.find_borders)
+        optimal_borders = images.minmax_borders(border_list)
+        cropped_images = images.batch_process(images.crop_image, optimal_borders)
+        stitched_img = images.stitch(cropped_images, COLS, ROWS)
+        images.save_image(stitched_img, OUTPUT)
+    except TypeError as e:
+        print(e)
